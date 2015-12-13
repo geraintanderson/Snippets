ddb6b8791e4055e1be4c72d970e798db3ab81122
Geraint Anderson <geraintanderson@users.noreply.github.com>
2015-12-13 15:47:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,33 +2,48 @@
 
   var app = angular.module('notesApp', []);
   app.controller('NotesController', function($scope){
+
     $scope.notes = notes;
-    $scope.activeNoteNum = 0;
-    //$scope.activeNote = notes[$scope.activeNoteNum];
-    $scope.activeNote = angular.copy(notes[$scope.activeNoteNum]);
+    $scope.noteIndex = 0;
+    $scope.activeNote = angular.copy($scope.notes[$scope.noteIndex]);
+
     $scope.onSave = function(){
-      alert('Saved');
+      $scope.notes[$scope.noteIndex] = $scope.activeNote;
     };
+
     $scope.onClear = function(){
       $scope.activeNote.text = "";
     };
-    $scope.setNote = function(noteId){
-      $scope.activeNoteNum = noteId;
-      $scope.activeNote = angular.copy(notes[$scope.activeNoteNum]);
+
+    $scope.deleteNote = function(){
+      $scope.notes.splice($scope.noteIndex, 1);
     };
+
+    $scope.newNote = function(){
+      var newNoteId = $scope.notes[$scope.notes.length - 1 ].id + 1;
+      $scope.notes.push({id:newNoteId, text:'New Note'});
+    };
+
+    $scope.setNote = function(noteIndex){
+      $scope.noteIndex = noteIndex;
+      $scope.activeNote = angular.copy($scope.notes[noteIndex]);
+
+    };
+
   });
+
 
   var note1 = {
     id: 0,
-    text: "abc",
+    text: "Note0",
   };
   var note2 = {
     id: 1,
-    text: "Note2",
+    text: "Note1",
   };
   var note3 = {
     id: 2,
-    text: "Note3",
+    text: "Note2",
   };
 
   var notes = [note1, note2, note3];
